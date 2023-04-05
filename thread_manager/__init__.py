37f8d21c343ff6dec579bce917e4a9ecf7c91599
Raynor <jonggu.woo@fitpet.co.kr>
2023-04-05 14:37:08 +0900

    🩹 fix bugs

--- thread_manager/__init__.py
+++ thread_manager/__init__.py
@@ -1,7 +1,2 @@
-import logging
-
 from thread_manager.argument import ThreadArgument
 from thread_manager.manager import ThreadManager
-
-# logger = logging.getLogger("py.thread.manager")
-# logger.setLevel(logging.INFO)
