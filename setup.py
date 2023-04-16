8588b0416dcad0568cb4b503bf0628d452cdafe6
Raynor <jonggu.woo@fitpet.co.kr>
2023-04-17 00:23:09 +0900

    ✨ upload new version into pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='thread-manager-py',
-    version='0.2.0',
+    version='0.3.0',
     url='https://github.com/sanggi-wjg/py-thread-manager',
     author='SangGi',
     author_email='girr311@naver.com',
